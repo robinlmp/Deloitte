45dab82d80c6430bf5c90c6bc52788bc3814b80e
Robin Phillips <robinlmp@mac.com>
2021-12-11 20:52:13 +0000

    Added total and formatted text and added $ sign

--- Clothes Store/Controllers/BasketViewController.swift
+++ Clothes Store/Controllers/BasketViewController.swift
@@ -32,10 +32,10 @@ class BasketViewController: UIViewController, UITableViewDataSource {
 
         checkoutButton.dropShadow(radius: 8, opacity: 0.4, color: UIColor.primaryColour)
         
+        total.text = "$" + String(format: "%.2f", Basket.items.map( { $0.product.price ?? 0 } ).reduce(Float(0), +))
         
         print(Basket.items.description)
-        
- 
+
     }
     
     
@@ -74,37 +74,37 @@ extension BasketViewController: UITableViewDelegate{
     }
     
 
-
-
-func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 125
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
+        
         tableView.deselectRow(at: indexPath, animated: true)
-
+        
     }
-
+    
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         
         let deleteAction = UIContextualAction.init(style:.destructive, title: "Remove", handler: { (action, view, completion) in
             
             self.productsArray.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-
+            
             Haptic.feedBack()
         })
-
+        
         deleteAction.backgroundColor = UIColor.primaryColour
-
+        
         let config = UISwipeActionsConfiguration(actions: [deleteAction])
         config.performsFirstActionWithFullSwipe = true
         return config
-
+        
     }
-    
 
+    
 }
 
 
