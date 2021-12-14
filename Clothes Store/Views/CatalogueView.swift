d24c0aee1224e6a0ed57fe9edc57f982664bbfa0
Robin Phillips <robinlmp@mac.com>
2021-12-14 02:41:32 +0000

    Split out card to new view

--- Clothes Store/Views/CatalogueView.swift
+++ Clothes Store/Views/CatalogueView.swift
@@ -44,6 +44,8 @@ struct CatalogueView: View {
                             ProductCardView(product: product)
                         }
                     }
+                    .padding(.vertical, 20)
+                    .padding(.horizontal, 10)
                 }
                 
                 VStack {
@@ -79,47 +81,44 @@ struct CatalogueView_Previews: PreviewProvider {
     }
 }
 
-struct ProductCardView: View {
-    let customGrey = Color(uiColor: UIColor(red: 85/255, green: 85/255, blue: 85/255, alpha: 1))
-    let product: Product
-    
-    var body: some View {
-        ZStack {
-            Rectangle()
-                .foregroundColor(.white)
-            VStack(spacing: 0) {
-                AsyncImage(url: URL(string: product.image ?? "www.apple.com") ) { image in
-                    image
-                        .resizable()
-                        .scaledToFit()
-                } placeholder: {
-                    Image("photo.placeholder")
-                }
-                .padding(8)
-                .padding(.top, 2)
-                
-                HStack {
-                    VStack(alignment: .leading, spacing: 0) {
-                        Text(product.name ?? "")
-                            .font(.footnote)
-                            .fontWeight(.thin)
-                            .foregroundColor(customGrey)
-                            
-                        Text(CurrencyHelper.getMoneyString(product.price ?? 0.0))
-                            .fontWeight(.bold)
-                            .foregroundColor(customGrey)
-                            .padding(.top, 15)
-                            
-                    }
-                    Spacer()
-                }.padding(7)
-                
-
-            }
-        }
-        .frame(maxWidth: 150, maxHeight: 220)
-        .clipShape(RoundedRectangle(cornerRadius: 10))
-        .shadow(color: .gray.opacity(0.3), radius: 15, x: 0, y: 0)
-//        .padding()
-    }
-}
+//struct ProductCardView: View {
+//    let customGrey = Color(uiColor: UIColor(red: 85/255, green: 85/255, blue: 85/255, alpha: 1))
+//    let product: Product
+//    
+//    var body: some View {
+//        ZStack {
+//            Rectangle()
+//                .foregroundColor(.white)
+//            VStack(spacing: 0) {
+//                AsyncImage(url: URL(string: product.image ?? "www.apple.com") ) { image in
+//                    image
+//                        .resizable()
+//                        .scaledToFit()
+//                } placeholder: {
+//                    Image("photo.placeholder")
+//                }
+//                .padding(8)
+//                .padding(.top, 2)
+//                
+//                HStack {
+//                    VStack(alignment: .leading, spacing: 0) {
+//                        Text(product.name ?? "")
+//                            .font(.footnote)
+//                            .fontWeight(.thin)
+//                            .foregroundColor(customGrey)
+//                            
+//                        Text(CurrencyHelper.getMoneyString(product.price ?? 0.0))
+//                            .fontWeight(.bold)
+//                            .foregroundColor(customGrey)
+//                            .padding(.top, 15)
+//                            
+//                    }
+//                    Spacer()
+//                }.padding(7)
+//            }
+//        }
+//        .frame(maxWidth: 150, maxHeight: 220)
+//        .clipShape(RoundedRectangle(cornerRadius: 10))
+//        .shadow(color: .gray.opacity(0.3), radius: 15, x: 0, y: 0)
+//    }
+//}
