e1139fe7aff64d8bc99a7e2f8544071f258c04be
Robin Phillips <robinlmp@mac.com>
2021-12-14 02:33:32 +0000

    LazyVGrid working and looking pretty good

--- Clothes Store/Views/CatalogueView.swift
+++ Clothes Store/Views/CatalogueView.swift
@@ -22,6 +22,8 @@ struct CatalogueView: View {
     let appBackgroundColour = Color(UIColor(red: 243/255, green: 243/255, blue: 243/255, alpha: 1))
     let appBackgroundColour2 = Color(UIColor(red: 218/255, green: 218/255, blue: 218/255, alpha: 0.9))
     
+    let gridItem = [GridItem(), GridItem()]
+    
     @State private var productsArray: [Product] = []
     
     var body: some View {
@@ -29,11 +31,19 @@ struct CatalogueView: View {
             ZStack {
                 Rectangle().foregroundColor(appBackgroundColour)
                 
+//                ScrollView {
+//                    ForEach(productsArray, id: \.self) { product in
+//                        ProductCardView(product: product)
+//                    }
+//                    .frame(maxWidth: .infinity)
+//                }
+                
                 ScrollView {
-                    ForEach(productsArray, id: \.self) { product in
-                        ProductCardView(product: product)
+                    LazyVGrid(columns: gridItem, spacing: 35) {
+                        ForEach(productsArray, id: \.self) { product in
+                            ProductCardView(product: product)
+                        }
                     }
-                    .frame(maxWidth: .infinity)
                 }
                 
                 VStack {
@@ -70,12 +80,46 @@ struct CatalogueView_Previews: PreviewProvider {
 }
 
 struct ProductCardView: View {
+    let customGrey = Color(uiColor: UIColor(red: 85/255, green: 85/255, blue: 85/255, alpha: 1))
     let product: Product
+    
     var body: some View {
-        RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
-            .foregroundColor(.white)
-            .frame(minWidth: 150, minHeight: 220)
-            .padding()
-            .shadow(color: .gray.opacity(0.3), radius: 15, x: 0, y: 0)
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+            VStack(spacing: 0) {
+                AsyncImage(url: URL(string: product.image ?? "www.apple.com") ) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    Image("photo.placeholder")
+                }
+                .padding(8)
+                .padding(.top, 2)
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text(product.name ?? "")
+                            .font(.footnote)
+                            .fontWeight(.thin)
+                            .foregroundColor(customGrey)
+                            
+                        Text(CurrencyHelper.getMoneyString(product.price ?? 0.0))
+                            .fontWeight(.bold)
+                            .foregroundColor(customGrey)
+                            .padding(.top, 15)
+                            
+                    }
+                    Spacer()
+                }.padding(7)
+                
+
+            }
+        }
+        .frame(maxWidth: 150, maxHeight: 220)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .shadow(color: .gray.opacity(0.3), radius: 15, x: 0, y: 0)
+//        .padding()
     }
 }
